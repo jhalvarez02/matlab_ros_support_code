477e9beef3a3b29b34a82c88e76042a6e4687705
jhalvarez02 <jahernandezalvarez@mail.lipscomb.edu>
2024-04-02 15:29:30 -0500

    HW9

--- actions/moveToBin.m
+++ actions/moveToBin.m
@@ -20,7 +20,7 @@ function grip_result = moveToBin(strategy,mat_current, mat_bin)
     ops("debug")               = 0;     % If set to true visualize traj before running  
     ops("toolFlag")            = 0;     % Include rigidly attached robotiq fingers
     ops("traj_steps")          = 1;     % Num of traj steps
-    ops("z_offset")            = 0.1;   % Vertical offset for top-down approach
+    ops("z_offset")            = 0.3;   % Vertical offset for top-down approach
     ops("traj_duration")       = 2;     % Traj duration (secs)   
 
     grip_result                = -1;           % Init to failure number  
